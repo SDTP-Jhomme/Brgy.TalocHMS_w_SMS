55acfd1e13ef5c4e5f99ba8d98f0f1b0d191636e
jhomme123 <dimasuayjhomme3@gmail.com>
2022-07-16 19:01:55 +0800

    7/16

--- index.php
+++ index.php
@@ -3,12 +3,19 @@
     <head>
         <meta charset="UTF-8">
         <title>Health Management | Login</title>
+        <link rel="stylesheet" type="text/css" href="./style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </head>
     <body>
-        <div class="text-center container-xxl">
-            <p>container-xxl"</p>
+        <div class="index-page">
+            <div class="container-fluid d-flex justify-content-between links">
+                <p>Barangay Taloc Online Health Record Management System with SMS Notification</p>
+                <a href="./login">Login</a>
+            </div>
+            <div class="container-fluid footer">
+                <p>Copyright © 2022 | Barangay Taloc Online Health Record Management System | All Rights Reserved | <a href="./about">About</a></p>
+            </div>
         </div>
     </body>
 </html>
