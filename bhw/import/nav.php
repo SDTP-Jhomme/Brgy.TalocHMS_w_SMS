47e9a361f96479f1cbf2b825ae5a8db87b99240c
jhomme123 <dimasuayjhomme3@gmail.com>
2022-10-08 13:59:27 +0800

    10/8

--- bhw/import/nav.php
+++ bhw/import/nav.php
@@ -21,7 +21,7 @@
             </a>
             <!-- Dropdown - User Information -->
             <div id="dropdownMenu" class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">
+                <a class="dropdown-item" href="./profile">
                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                     Profile
                 </a>
---
+++ bhw/profile.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>BHW | Add Patient</title>
+    <?php
+
+    include("./import/head.php");
+
+    if (isset($_SESSION["user_id"])) {
+
+        $id = $_SESSION["user_id"];
+
+        $user_record = mysqli_query($db, "SELECT * FROM users where id='$id'");
+
+        $user_row = mysqli_fetch_assoc($user_record);
+
+        $db_username = $user_row["username"];
+        $db_last_login = $user_row["last_login"];
+        $logged_user = ucfirst($db_username);
+        $db_avatar = $user_row["avatar"];
+
+        if ($db_last_login == "") {
+            header("Location: ./change-password");
+        }
+    } else {
+
+        header("Location: ../../capstone-new");
+        die();
+    } ?>
+</head>
+
+<body id="page-top">
+    <!-- Page Wrapper -->
+    <div id="app" v-loading.fullscreen.lock="fullscreenLoading">
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column vh-100">
+            <!-- Main Content -->
+            <div id="content">
+                <!-- Topbar -->
+                <?php include("./import/nav.php"); ?>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+                    <!-- ElementUI Container -->
+                    <?php include("./user-profile.php"); ?>
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+            <!-- Footer -->
+            <?php include("./import/footer.php") ?>
+            <!-- End of Footer -->
+        </div>
+        <!-- End of Content Wrapper -->
+    </div>
+    <!-- End of Page Wrapper -->
+    <?php include("./import/body.php") ?>
+    <script>
+        ELEMENT.locale(ELEMENT.lang.en)
+        new Vue({
+            el: "#app",
+            data() {
+                return {
+                    fullscreenLoading: true
+                }
+            },
+            mounted() {
+                setTimeout(() => {
+                    this.fullscreenLoading = false
+                }, 1000)
+            },
+            methods: {
+                // Logout **********************************************************
+                logout() {
+                    this.fullscreenLoading = true
+                    axios.post("auth.php?action=logout")
+                        .then(response => {
+                            if (response.data.message) {
+                                this.$notify({
+                                    title: 'Success',
+                                    message: 'Successfully logged out!',
+                                    type: 'success'
+                                });
+                                setTimeout(() => {
+                                    window.location.href = "../../capstone-new"
+                                }, 1000)
+                            }
+                        })
+                },
+                // ******************************************************************
+            }
+        })
+    </script>
+</body>
+
+</html>
