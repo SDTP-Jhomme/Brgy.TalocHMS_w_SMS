a5745009f26425339af099d3ac53414492bd98dc
jhomme123 <dimasuayjhomme3@gmail.com>
2022-10-09 17:31:52 +0800

    10/09

--- database/database.php
+++ database/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = mysqli_connect("localhost", "root", "", "capstone_new");
+$db = mysqli_connect("localhost", "root", "", "capstone");
 
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: ".mysqli_connect_errno();
